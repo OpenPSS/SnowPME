2be20ddd0eef69a03852b2c6837f52e3975f9ed7
Li <li@silica.codes>
2025-04-13 12:07:21 +1200

    add libmonobridge

--- subprojects/LibMonoBridge/LibMonoBridge.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef LIB_MONO_BRIDGE_H
-#define LIB_MONO_BRIDGE_H 1
-
-#include <PssCrypto.h>
-#include <PssCryptoContext.h>
-#include <PssCryptoCallbacks.h>
-#include <PssCryptoSeek.h>
-
-#endif
