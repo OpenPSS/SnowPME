1ed7dfc59968082bc7e0c441d20216ce6e62490c
Li <li@silica.codes>
2025-06-06 01:12:19 +1200

    add a mutex

--- subprojects/LibPSM/Sce/Pss/Core/PsmUniqueObject.hpp
+++ subprojects/LibPSM/Sce/Pss/Core/PsmUniqueObject.hpp
@@ -1,22 +1,26 @@
 #ifndef LIB_PSS_PSM_UNIQUE_OBJECT_H
 #define LIB_PSS_PSM_UNIQUE_OBJECT_H 1
 #include <Sce/Pss/Core/Errorable.hpp>
+#include <Sce/Pss/Core/PsmMutexObject.hpp>
 
 namespace Sce::Pss::Core {
-	template<typename T> class PsmUniqueObject {
+	template<typename T> class PsmUniqueObject : public PsmMutexObject<T> {
 	private:
 		static T* uniqueObjectPtr;
 	public:
 		PsmUniqueObject() {
+			LOCK_GUARD();
 			assert(!PsmUniqueObject<T>::Exists());
 			PsmUniqueObject<T>::uniqueObjectPtr = reinterpret_cast<T*>(this);
 		}
 
 		~PsmUniqueObject() {
+			LOCK_GUARD();
 			uniqueObjectPtr = nullptr;
 		}
 
 		static T* GetUniqueObject() {
+			LOCK_GUARD_STATIC();
 			return PsmUniqueObject<T>::uniqueObjectPtr;
 		}
 
