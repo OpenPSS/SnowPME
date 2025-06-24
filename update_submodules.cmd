935bb7cd62af29c076fee6cb654b22544f767fad
Li <li@silica.codes>
2025-06-24 15:57:40 +1200

    set warning level to 0 for LibPSM

--- update_submodules.cmd
+++ update_submodules.cmd
@@ -2,25 +2,25 @@
 cd subprojects/LibCXML
 git checkout main
 git add -A 
-git commit -m %%1
+git commit -m "%1"
 git push
 cd ../..
 
 cd subprojects/LibPssMono/pss-mono
 git checkout mono-2-11
 git add -A 
-git commit -m %%1
+git commit -m "%1"
 git push
 cd ../../..
 
 cd subprojects/LibMonoBridge
 git checkout main
 git add -A 
-git commit -m %%1
+git commit -m "%1"
 git push
 cd ../..
 
 git submodule update --remote --merge
 git add -A
-git commit -m "%%1"
+git commit -m "%1"
 git push
