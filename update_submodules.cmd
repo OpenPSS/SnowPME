c189e19cd35ccd2ad6dc7b68bd4d5d8a7ab9f946
Li <li@silica.codes>
2025-06-24 15:52:24 +1200

    %1

--- update_submodules.cmd
+++ update_submodules.cmd
@@ -1,1 +1,20 @@
+cd subprojects/LibCXML
+git checkout main
+git add -A 
+git commit -m %%1
+git push
+cd ..
+cd subprojects/LibPssMono/pss-mono
+git checkout mono-2-11
+git add -A 
+git commit -m %%1
+git push
+cd ../..
+cd subprojects/LibMonoBridge
+git checkout main
+git add -A 
+git commit -m %%1
+git push
+cd ../..
+
 git submodule update --remote --merge
