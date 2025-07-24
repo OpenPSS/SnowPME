16df1419da9191acfee778c2004dc6f2179d9b59
Li <li@silica.codes>
2025-07-24 20:22:15 +1200

    Fix --game, -g direct specify a program

--- SnowPME/Program.cpp
+++ SnowPME/Program.cpp
@@ -2,6 +2,7 @@
 #include <LibShared.hpp>
 #include <LibSnowPME.hpp>
 
+#include <filesystem>
 #include <cxxopts.hpp>
 using namespace Shared;
 using namespace Shared::Debug;
@@ -25,7 +26,7 @@ namespace SnowPME {
 		}
 
 		Logger::Debug("Running mono program ...");
-		if (ProgramSelectWindow::Programs.HasSelectedProgram()) {
+		if (!this->programPath.empty()) {
 			this->exitCode = Application::LoadApplication(this->programPath);
 		}
 
@@ -46,19 +47,23 @@ namespace SnowPME {
 			gui->RenderGui();
 		}
 		
-		this->programPath = ProgramSelectWindow::Programs.SelectedProgram().programPath;
+		if(ProgramSelectWindow::Programs.HasSelectedProgram()) this->programPath = ProgramSelectWindow::Programs.SelectedProgram().programPath;
 		this->progThreadFunc();
 	}
 
 	Program::Program(int argc, const char* const* argv) {
-		std::string runningFrom = "";
-		Config::ReadConfig(runningFrom, "SnowPME.cfg");
 
+		// always run from the executable directory;
+		std::string runDir;
+		if (argc >= 1) runDir = std::filesystem::absolute(argv[0]).parent_path().string();
+		if (!std::filesystem::exists(runDir)) runDir = "";
+
+		Config::ReadConfig(runDir, "SnowPME.cfg");
 		cxxopts::Options options("snowpme", "SnowPME PlayStation Mobile Emulator");
 
 		options.add_options()
-			("p,path", "Game Path", cxxopts::value<std::string>())
-			("g,gui", "show gui", cxxopts::value<bool>())
+			("g,game", "Game Path", cxxopts::value<std::string>())
+			("s,showgui", "Show gui", cxxopts::value<bool>())
 			("h,help", "Print usage");
 
 		auto opts = options.parse(argc, argv);
@@ -73,7 +78,7 @@ namespace SnowPME {
 		ApplicationEvent::Init();
 
 
-		auto gamePath = opts["path"].as_optional<std::string>();
+		auto gamePath = opts["game"].as_optional<std::string>();
 		if (gamePath.has_value()) {
 			this->programPath = gamePath.value();
 			this->guiThread = std::thread(&Program::progThreadFunc, this);
@@ -81,8 +86,8 @@ namespace SnowPME {
 		else {
 			bool showGui = true;
 
-			if (opts.count("gui")) {
-				showGui = opts["gui"].as<bool>();
+			if (opts.count("showgui")) {
+				showGui = opts["showgui"].as<bool>();
 			}
 
 			if (showGui) {
