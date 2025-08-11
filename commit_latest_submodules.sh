700875e87ddb945333fa79e1d2a7fbc577159424
Li <li@silica.codes>
2025-08-11 15:20:30 +1200

    update pss-mono

---
+++ commit_latest_submodules.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+cd subprojects/LibCXML
+git checkout main
+git add -A 
+git commit -m $1
+git push
+cd ../..
+
+cd subprojects/LibPssMono/pss-mono
+git checkout mono-2-11
+git add -A 
+git commit -m $1
+git push
+cd ../../..
+
+cd subprojects/LibMonoBridge
+git checkout main
+git add -A 
+git commit -m $1
+git push
+cd ../..
+
+git submodule update --remote --merge
+git add -A
+git commit -m $1
+git push
