2a1a6005919ef518448ced7fd6b02d652e0fcc58
Li <li@silica.codes>
2026-09-06 18:21:04 +1200

    Fix OOB read

--- subprojects/LibPSM/Sce/Pss/Core/Graphics/ShaderProgram.cpp
+++ subprojects/LibPSM/Sce/Pss/Core/Graphics/ShaderProgram.cpp
@@ -254,12 +254,12 @@ namespace Sce::Pss::Core::Graphics {
 
 	void ShaderProgram::RemapParameters(ProgramUniform* entry, int total)
 	{
-		//int ii = 0;
+		int ii = 0;
 		//result = entry;
 
 		if (total <= 0) return;
 
-		for (int i = total; i != 0; i--)
+		for (int i = 0; i < total; i++)
 		{
 			entry[i].Index = -1;
 		}
@@ -277,7 +277,6 @@ namespace Sce::Pss::Core::Graphics {
 			if (entry[i].Binding < 0 || entry[i].Binding >= total)
 			{
 				// find first not -1
-				int ii = 0;
 				for (; entry[ii].Index >= 0; ++ii)
 
 				//result = (6 * ii); wtf seemingly not used?
